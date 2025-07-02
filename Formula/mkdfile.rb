3013658ab1fbff6053ad7d622352328056d389f1
Alan Ramalho <ramalho.sit@gmail.com>
2025-07-02 01:34:55 -0300

    Atualiza a descrição do Mkdfile e ajusta a URL de download para a versão 0.1.0

--- Formula/mkdfile.rb
+++ Formula/mkdfile.rb
@@ -1,16 +1,12 @@
 class Mkdfile < Formula
-  include Language::Python::Virtualenv
-
-  desc "Gerador de Dockerfile customizado"
+  desc "Gerador de Dockerfile com suporte a multistage"
   homepage "https://github.com/raioramalho/mkdfile"
-  url "https://github.com/raioramalho/mkdfile/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1a9031de1a293af147421760679fe6cb2d453b3f3793cee961d039ad911501dd"
+  url "https://github.com/raioramalho/mkdfile/releases/download/v0.1.0/mkdfile-macos.tar.gz"
+  sha256 "INSIRA_O_HASH_AQUI"
   license "MIT"
 
-  depends_on "python@3.12"
-
   def install
-    virtualenv_install_with_resources
+    bin.install "mkdfile"
   end
 
   test do
