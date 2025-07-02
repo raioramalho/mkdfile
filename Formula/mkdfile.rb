65128986ff8ebf3a5e3238eda8605545e76d306c
Alan Ramalho <ramalho.sit@gmail.com>
2025-07-02 01:45:48 -0300

    Atualiza o hash SHA256 no arquivo mkdfile.rb para a versão correta

--- Formula/mkdfile.rb
+++ Formula/mkdfile.rb
@@ -2,7 +2,7 @@ class Mkdfile < Formula
   desc "Gerador de Dockerfile com suporte a multistage"
   homepage "https://github.com/raioramalho/mkdfile"
   url "https://github.com/raioramalho/mkdfile/releases/download/v0.1.0/mkdfile-macos.tar.gz"
-  sha256 "0e849c52b7867998479ca04d4856b05d7865ffa00aa27b4977a8bba7b29da5a9"
+  sha256 "795e6ba84cc66e86db5c5c18b8d265ebbb3fe20469078487db8a3b1e7797309d"
   license "MIT"
 
   def install
